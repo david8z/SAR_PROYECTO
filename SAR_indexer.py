dbff9fea179bc37d6fc50fa227f8d53ccd8ba60b
David Alarcón <alarconsegarradavid@gmail.com>
2019-04-03 16:07:17 +0200

    Añdido pseudocodigo

--- SAR_indexer.py
+++ SAR_indexer.py
@@ -1,5 +1,17 @@
 import re
+
 clean_re = re.compile('\W+')
 
 def clean_text(text):
     return clean_re.sub(' ', text)
+
+
+# mientras hay_documentos:
+#     doc ← leer_siguiente_documento()
+#     docid ← asignar_identificador_al_doc()
+#     mientras hay_noticias_en_doc:
+#         noticia ← extraer_siguiente_noticia()
+#         newid ← asignar_identificador_a_la_noticia()
+#         noticia_limpia ← procesar_noticia(noticia)
+#         para termino en noticia_limpia:
+#             añadir_noticia_al_postings_list_del_termino(termino, newid)
