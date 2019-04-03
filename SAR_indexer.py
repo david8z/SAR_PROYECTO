6742026e306855e18a6cbeff7a2ac654ab473453
josepitarch <39901880+josepitarch@users.noreply.github.com>
2019-04-03 18:56:29 +0200

    Update SAR_indexer.py

--- SAR_indexer.py
+++ SAR_indexer.py
@@ -1,4 +1,7 @@
 import re
+import sys
+from os import scandir
+import json
 
 
 clean_re = re.compile('[_\W]+')
@@ -18,6 +21,45 @@ def add_to_posting_list(termino, newsId, pos):
 
 def add_to_news_table(docId, pos, newsId):
     news_table[newsId] = (docId, pos)
+
+def read_noticias(text):
+    with open(text) as json_file:
+        return json.load(json_file)
+        
+def indexar_noticias(dir_noticias, indice_fichero):
+    dicDoc = {}
+    docID = 0
+    notID = 0
+    #Lista de noticias que se encuentran en el directorio
+    documentos = [arch.name for arch in scandir(dir_noticias) if arch.is_file()]
+    #Recorremos todos los documentos eliminandolos de la lista
+    while len(documentos) > 0:
+        #Posicion de la noticia en el documento
+        posNot = 0
+        path = dir_noticias + '/' + documentos.pop(0)
+        dicDoc[docID] = path
+       noticias = read_noticias(path)
+        #Recorremos todas las noticias eliminandolas de la lista
+        for noticia in noticias:
+            text = clean_text(noticia['article']).split()
+            notID = noticia['id']
+            #Posicion del término en la noticia
+            posTer = 0
+            for termino in text:
+                add_to_posting_list(termino, notID, posTer)
+                posTer += 1
+            add_to_news_table(docID, posNot, notID)
+            posNot += 1
+            docID += 1
+                
+            
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        println('Formato: <SAR_indexer.py> <directorio_noticias> <índice_fichero>')
+    else:
+      dir_noticias = sys.argv[1]
+      indice_fichero = sys.argv[2]
+      indexar_noticias(dir_noticias, indice_fichero)    
 
 # mientras hay_documentos:
 #     doc ← leer_siguiente_documento()
