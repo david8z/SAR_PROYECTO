5ff3748b3e603d28b4301c48daab6f65bef28788
lucargir <lluis@lucargir.es>
2019-05-03 13:27:20 +0200

    Corrección sOr

--- SAR_searcher.py
+++ SAR_searcher.py
@@ -29,6 +29,7 @@ def search(query, posting_list, news_table):
 
     OJO: Aquí se convierten las palabras a minúsculas
     """
+    print(query)
     # Caso base solo queda el resultado
     if (len(query) == 1):
         return query
@@ -86,19 +87,19 @@ def sOr(a, b, posting_list, news_table):
     res = []
     while(posA < len(a) and posB < len(b)):
         if (a[posA][0] == b[posB][0]):
-            res.append(a[posA][0])
+            res.append(a[posA])
             posA += 1
             posB += 1
-        elif (a[posA][0] < b[posB][0]):
-            res.append(a[posA][0])
+        elif (a[posA] < b[posB]):
+            res.append(a[posA])
             posA += 1
-        elif (b[posB][0] < a[posA][0]):
-            res.append(b[posB][0])
+        elif (b[posB] < a[posA]):
+            res.append(b[posB])
             posB += 1
         for i in range(posA, len(a)):
-            res.append(a[i][0])
+            res.append(a[i])
         for i in range(posB, len(b)):
-            res.append(b[i][0])
+            res.append(b[i])
     return res
 
 # Obtener noticias (como objeto json) que estén en la lista de newsID
